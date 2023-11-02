4412cadb86727c6cf636754f2aef50c74a4df08c
Emma Gerigscott <elgerig@gmail.com>
2023-11-02 16:33:50 -0700

    add secret obj + tests passing

--- secret-handshake.js
+++ secret-handshake.js
@@ -1,8 +1,17 @@
-//
-// This is only a SKELETON file for the 'Secret Handshake' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const SECRETS = {
+  '1': 'wink',
+  '01': 'double blink',
+  '001': 'close your eyes',
+  '0001': 'jump'
+}
 
-export const commands = () => {
-  throw new Error('Remove this statement and implement this function');
+export const commands = (num) => {
+  const binary = num.toString(2);
+  let secret = []
+  for (let i = binary.length - 1; i >= 0; i--) {
+    console.log(binary[i])
+    secret.push(SECRETS[binary[i]])
+  }
+
+  return secret
 };
